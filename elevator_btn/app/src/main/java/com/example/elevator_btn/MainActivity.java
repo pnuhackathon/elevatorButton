670689d0f1cb15ef444359ce8fc68431a579a3a7
minjib <48316078+minjib@users.noreply.github.com>
2019-08-17 15:03:56 +0900

    updateDesign

--- elevator_btn/app/src/main/java/com/example/elevator_btn/MainActivity.java
+++ elevator_btn/app/src/main/java/com/example/elevator_btn/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.elevator_btn;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,23 +14,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button out_btn = (Button) findViewById(R.id.out_btn);
-        Button in_btn = (Button) findViewById(R.id.in_btn);
+        setContentView(R.layout.activity_out_btn);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView_main_menu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        out_btn.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, Out_btn_Activity.class);
-                startActivity(intent);
-            }
-        });
-        in_btn.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, In_btn_Activity.class);
-                startActivity(intent);
-            }
-        });
+
+                        // 어떤 메뉴 아이템이 터치되었는지 확인합니다.
+                        switch (item.getItemId()) {
+
+                            case R.id.menuitem_bottombar_inside:
+
+
+                                return true;
+
+                            case R.id.menuitem_bottombar_ouside:
+
+
+                                return true;
+
+                            case R.id.menuitem_bottombar_settings:
+
+
+                                return true;
+                        }
+                        return false;
+                    }
+
+
+                });
+
+
     }
 }
--- elevator_btn/app/src/main/java/com/example/elevator_btn/Out_btn_Activity.java
+++ elevator_btn/app/src/main/java/com/example/elevator_btn/Out_btn_Activity.java
@@ -1,7 +1,10 @@
 package com.example.elevator_btn;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Out_btn_Activity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class Out_btn_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_out_btn);
+
+
+
+
     }
 }
